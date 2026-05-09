15ffebb823add0b89599ae7d4d3c4a4e866727c8
AlwinRaymond-tech <alwinraym@gmail.com>
2026-05-10 00:07:33 +0530

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/favourite-student-list/",
+  base: "/Favourite-student-list/",
   plugins: [react()]
 });
